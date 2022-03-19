7a75e668691479bd1acd44ade42ea65693d64492
Sudarshan <mail2sudarshan.sharma@gmail.com>
2022-03-19 10:17:15 +0530

    first set of development

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { useGlobalContext } from './context'
+
+// components
+import Navbar from './Navbar'
+import CartContainer from './CartContainer'
 
 function App() {
+  const { loading } = useGlobalContext();
+
+  if(loading){
+    return (
+      <div className="loading">
+        <h1>loading...</h1>
+      </div>
+    )
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar />
+      <CartContainer />
+    </>
   );
 }
 
